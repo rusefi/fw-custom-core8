53fd9990d19517d449d9de0497e524d9f4b3b10e
GitHub git update Action <action@github.com>
2024-04-24 17:19:12 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_core8.h
+++ generated/controllers/generated/rusefi_generated_core8.h
@@ -1215,7 +1215,7 @@
 #define show_microRusEFI_presets false
 #define show_Proteus_presets false
 #define show_test_presets true
-#define SIGNATURE_HASH 1147290101
+#define SIGNATURE_HASH 2119953385
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1594,7 +1594,7 @@
 #define ts_show_vr_threshold_all false
 #define ts_show_vr_threshold_pins true
 #define ts_show_vvt_output true
-#define TS_SIGNATURE "rusEFI default.2024.04.23.core8.1147290101"
+#define TS_SIGNATURE "rusEFI default.2024.04.24.core8.2119953385"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_core8.h
+++ generated/controllers/generated/signature_core8.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1147290101
-#define TS_SIGNATURE "rusEFI default.2024.04.23.core8.1147290101"
+#define SIGNATURE_HASH 2119953385
+#define TS_SIGNATURE "rusEFI default.2024.04.24.core8.2119953385"
