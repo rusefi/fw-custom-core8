69f7ee16c92a1732d03a4a45213e77647bab1d5d
GitHub git update Action <action@github.com>
2024-08-09 17:17:52 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_core8.h
+++ generated/controllers/generated/signature_core8.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3501182701
-#define TS_SIGNATURE "rusEFI main.2024.08.08.core8.3501182701"
+#define SIGNATURE_HASH 1447973440
+#define TS_SIGNATURE "rusEFI main.2024.08.09.core8.1447973440"
